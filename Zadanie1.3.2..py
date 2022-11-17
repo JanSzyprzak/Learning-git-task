40a6b177ed0efc43e1886a40fb11b623d6b95db1
Jan Szyprzak <jan.szyprzak@gmail.com>
2022-11-17 10:32:03 +0100

    Capitalize first letters in values

--- Zadanie1.3.2..py
+++ Zadanie1.3.2..py
@@ -1,3 +1,9 @@
 print("Lista zakupów:")
 print()
 lista_zakupow = {"piekarnia": ["chleb", "bułki", "pączek"], "warzywniak": ["marchew", "seler", "rukola"]}
+piekarnia = lista_zakupow.get("piekarnia")
+warzywniak = lista_zakupow.get("warzywniak")
+for i in range(len(piekarnia)):
+    piekarnia[i] = piekarnia[i].title()
+for i in range(len(warzywniak)):
+    warzywniak[i] = warzywniak[i].title() 
